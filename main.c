aa246854f6588cb40f557359f9b213885cb8f2a4
SergeyLavrinenko <l.s.a00@mail.ru>
2020-10-28 21:05:12 +0700

    First

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void){
+	int numberList[10];
+	
+	for(int i = 0; i < 10; i++){
+		if (i == 4 || i == 5)
+			numberList[i] = 3 - i % 2;
+		else
+			numberList[i] = i % 2;
+	}
+	
+	int i = 0;
+	while(i < 10){
+		printf("%d ", numberList[i]);
+		i++;
+	}
+	
+	return 0;
+}
